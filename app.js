5fe51f5784958f11dfbb22a960bc4503ac831069
Yuzi Lyu <yul134@ucsd.edu>
2021-05-08 13:31:57 -0700

    first draft

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let facebookButton = document.getElementById("facebook");
+    let instaButton = document.getElementById("instagram");
+    let gmailButton = document.getElementById("gmail");
+    let discordButton = document.getElementById("discord");
+    
+    
+   
+
+
+});
+
